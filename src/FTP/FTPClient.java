90404ce9a5053185194ffdb44a44f384dcae49c1
zhangjinxue <zhangjinxue@d05cdb54-5310-11de-8950-a5d9c972bc64>
2009-06-10 09:47:45 +0000

    The main functions for FTP client command is finished and tested. Now it is the time to add the UI

--- src/FTP/FTPClient.java
+++ src/FTP/FTPClient.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -21,6 +22,7 @@ import java.util.StringTokenizer;
  * <a href="http://www.jibble.org/">http://www.jibble.org/ </a>
  * 
  * Zhang Jinxue has extended this client
+ * Notice that there is a FtpClient library in Java standard library
  */
 
 public class FTPClient {
@@ -28,34 +30,43 @@ public class FTPClient {
 	private Socket socket = null;
 	private BufferedReader reader = null;
 	private BufferedWriter writer = null;
-	private static boolean DEBUG = false;
+	private static boolean DEBUG = true;
 
+	// This is socket for data transmission
+	private Socket dataSocket = null;
 	/**
 	 * Create an instance of SimpleFTP.
 	 */
 	public FTPClient() {
 	}
 
+	/*
+	 * Connect, cwd, cdup, quit is belong to Access Control commands in FTP protocol
+	 */
 	/**
 	 * Connects to the default port of an FTP server and logs in as
 	 * anonymous/anonymous.
 	 */
-	public synchronized void connect(String host) throws IOException {
+	public void connect(String host) throws IOException {
 		connect(host, 21);
 	}
 
 	/**
 	 * Connects to an FTP server and logs in as anonymous/anonymous.
 	 */
-	public synchronized void connect(String host, int port) throws IOException {
+	public void connect(String host, int port) throws IOException {
 		connect(host, port, "anonymous", "anonymous");
 	}
 
+	public void connect(String host, String user,
+			String pass) throws IOException {
+		connect(host, 21, user, pass);
+	}
 	/**
 	 * Connects to an FTP server and logs in with the supplied username and*
 	 * password.
 	 */
-	public synchronized void connect(String host, int port, String user,
+	public void connect(String host, int port, String user,
 			String pass) throws IOException {
 		if (socket != null) {
 			throw new IOException(
@@ -92,7 +103,7 @@ public class FTPClient {
 	/**
 	 * Disconnects from the FTP server.
 	 */
-	public synchronized void disconnect() throws IOException {
+	public void disconnect() throws IOException {
 		try {
 			sendLine("QUIT");
 		} finally {
@@ -101,9 +112,78 @@ public class FTPClient {
 	}
 
 	/**
+	 * Changes the working directory (like cd). Returns true if successful.
+	 */
+	public boolean cwd(String dir) throws IOException {
+		sendLine("CWD " + dir);
+		String response = readLine();
+		return (response.startsWith("250 "));
+	}
+
+	/*
+	 * Change the working directory to parent 
+	 */
+	public boolean cdup() throws IOException{
+		sendLine("CDUP ");
+		String response = readLine();
+		return (response.startsWith("250 "));
+	}
+	
+	/*
+	 *  PASV and PORT are both Transfer Parameter Commands in FTP protocol
+	 *  Here we only implement the PASV mode
+	 */
+	
+	/*
+	 * Set the data transport to as passive mode to avoid NAT or
+	 * firewall problems at the client end
+	 */
+	public boolean setPasv() throws IOException{
+//		if((dataSocket != null) && dataSocket.isConnected()){
+//			System.out.println("The data socket is established!");
+//			return true;
+//		}else{
+			sendLine("PASV");
+			String response = readLine();
+			if (!response.startsWith("227 ")) {
+				throw new IOException(
+						"FTPClient could not request passive mode: " + response);
+			}
+			String ip = null;
+			int port = -1;
+			int opening = response.indexOf('(');
+			int closing = response.indexOf(')', opening + 1);
+			if (closing > 0) {
+				String dataLink = response.substring(opening + 1, closing);
+				StringTokenizer tokenizer = new StringTokenizer(dataLink, ",");
+				try {
+					ip = tokenizer.nextToken() + "." + tokenizer.nextToken()
+							+ "." + tokenizer.nextToken() + "."
+							+ tokenizer.nextToken();
+					port = Integer.parseInt(tokenizer.nextToken()) * 256
+							+ Integer.parseInt(tokenizer.nextToken());
+				} catch (Exception e) {
+					throw new IOException(
+							"FTPClient received bad data link information: "
+									+ response);
+				}
+			}
+			dataSocket = new Socket(ip, port);
+			// boolean co = dataSocket.isConnected();
+			// co = dataSocket.isClosed();
+			return true;
+		}
+//	}
+	
+	/**
+	 * PWD, STOR, RETR, LIST, DELE, RMD, MKD, RNFR/RMTO, and ABOR are the FTP
+	 * Service Commands
+	 */
+	
+	/**
 	 * Returns the working directory of the FTP server it is connected to.
 	 */
-	public synchronized String pwd() throws IOException {
+	public String pwd() throws IOException {
 		sendLine("PWD");
 		String dir = null;
 		String response = readLine();
@@ -118,20 +198,45 @@ public class FTPClient {
 	}
 
 	/**
-	 * Changes the working directory (like cd). Returns true if successful.
+	 * List the directory or file information  
+	 * @throws IOException 
+	 * @throws IOException 
 	 */
-	public synchronized boolean cwd(String dir) throws IOException {
-		sendLine("CWD " + dir);
-		String response = readLine();
-		return (response.startsWith("250 "));
+	public boolean list() throws IOException{
+		return list("./");
 	}
-
+	
+	public boolean list(String filename) throws IOException {
+		setPasv();
+		if(filename.equals("./"))
+			sendLine("LIST ");
+		else
+			sendLine("LIST " + filename);
+		
+		String response = readLine();
+		if (!response.startsWith("150 ")) {
+			throw new IOException(
+					"FTPClient was not allowed to get the directory list: " + response);
+		}
+		//BufferedInputStream input = new BufferedInputStream(dataSocket.getInputStream());
+		BufferedReader dataReader = new BufferedReader(new InputStreamReader(dataSocket
+				.getInputStream()));
+		String totalInfo = new String();
+		String info = null;
+		while((info = dataReader.readLine()) != null){
+			totalInfo += info + "\r\n";
+		}
+		if(DEBUG)
+			System.out.println(totalInfo);
+		response = readLine();
+		return (response.startsWith("226 "));
+	}
+	
 	/**
 	 * Sends a file to be stored on the FTP server. Returns true if the file
-	 * transfer was successful. The file is sent in passive mode to avoid NAT or
-	 * firewall problems at the client end.
+	 * transfer was successful.
 	 */
-	public synchronized boolean stor(File file) throws IOException {
+	public boolean stor(File file) throws IOException {
 		if (file.isDirectory()) {
 			throw new IOException("FTPClient cannot upload a directory.");
 		}
@@ -144,36 +249,15 @@ public class FTPClient {
 	 * transfer was successful. The file is sent in passive mode to avoid NAT or
 	 * firewall problems at the client end.
 	 */
-	public synchronized boolean stor(InputStream inputStream, String filename)
+	public boolean stor(InputStream inputStream, String filename)
 			throws IOException {
 		BufferedInputStream input = new BufferedInputStream(inputStream);
-		sendLine("PASV");
-		String response = readLine();
-		if (!response.startsWith("227 ")) {
-			throw new IOException("FTPClient could not request passive mode: "
-					+ response);
-		}
-		String ip = null;
-		int port = -1;
-		int opening = response.indexOf('(');
-		int closing = response.indexOf(')', opening + 1);
-		if (closing > 0) {
-			String dataLink = response.substring(opening + 1, closing);
-			StringTokenizer tokenizer = new StringTokenizer(dataLink, ",");
-			try {
-				ip = tokenizer.nextToken() + "." + tokenizer.nextToken() + "."
-						+ tokenizer.nextToken() + "." + tokenizer.nextToken();
-				port = Integer.parseInt(tokenizer.nextToken()) * 256
-						+ Integer.parseInt(tokenizer.nextToken());
-			} catch (Exception e) {
-				throw new IOException(
-						"FTPClient received bad data link information: "
-								+ response);
-			}
-		}
+		
+		// Set the passive mode for data transmission
+		setPasv();
+		
 		sendLine("STOR " + filename);
-		Socket dataSocket = new Socket(ip, port);
-		response = readLine();
+		String response = readLine();
 		if (!response.startsWith("150 ")) {
 			throw new IOException(
 					"FTPClient was not allowed to send the file: " + response);
@@ -193,9 +277,94 @@ public class FTPClient {
 	}
 
 	/**
+	 * Send the file to the server
+	 * @throws IOException 
+	 */
+	public boolean download(String filename) throws IOException{
+		// Set the passive mode for data transmission
+		setPasv();
+		
+		sendLine("RETR " + filename);
+		String response = readLine();
+		if(response.startsWith("550 ")){
+			return false;
+		}
+		if (!response.startsWith("150 ")) {
+			throw new IOException(
+					"FTPClient was not allowed to download the file: " + response);
+		}
+		BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(new File(filename)));
+
+		BufferedInputStream input = new BufferedInputStream(dataSocket
+				.getInputStream());
+		byte[] buffer = new byte[4096];
+		int bytesRead = 0;
+		while ((bytesRead = input.read(buffer)) != -1) {
+			output.write(buffer, 0, bytesRead);
+		}
+		output.flush();
+		output.close();
+		input.close();
+
+		response = readLine();
+		return (response.startsWith("226 "));
+	}
+	
+	/**
+	 * Delete a file in server
+	 * @throws IOException 
+	 */
+	public boolean delete(String filename) throws IOException{
+		sendLine("DELE " + filename);
+		String response = readLine();
+		return (response.startsWith("250 "));
+	}
+	
+	/**
+	 * make the directory in server
+	 */
+	public boolean mkDirectory(String dir) throws IOException{
+		sendLine("MKD " + dir);
+		String response = readLine();
+		return (response.startsWith("250 "));
+	}
+	
+	/**
+	 * Remove the directory in server
+	 */
+	public boolean rmDirectory(String dir) throws IOException{
+		sendLine("RMD " + dir);
+		String response = readLine();
+		return (response.startsWith("250 "));
+	}
+
+	/**
+	 * Tell the server to abort the previous command and data transmission related
+	 */
+	public boolean abort(String dir) throws IOException{
+		sendLine("ABOR");
+		String response = readLine();
+		return (response.startsWith("250 "));
+	}
+	
+	/**
+	 * Rename the file in server
+	 */
+	public boolean rename(String oldName, String newName) throws IOException{
+		sendLine("RNFR " + oldName);
+		String response = readLine();
+		if(!response.startsWith("350 "))
+			return false;
+		
+		sendLine("RNTO " + newName);
+		response = readLine();
+		return (response.startsWith("250 "));
+	}
+	
+	/**
 	 * Enter binary mode for sending binary files.
 	 */
-	public synchronized boolean bin() throws IOException {
+	public boolean bin() throws IOException {
 		sendLine("TYPE I");
 		String response = readLine();
 		return (response.startsWith("200 "));
@@ -206,7 +375,7 @@ public class FTPClient {
 	 * mode. Make sure you use binary mode if you are sending images or other
 	 * binary data, as ASCII mode is likely to corrupt them.
 	 */
-	public synchronized boolean ascii() throws IOException {
+	public boolean ascii() throws IOException {
 		sendLine("TYPE A");
 		String response = readLine();
 		return (response.startsWith("200 "));
---
+++ src/FTP/FTPClientTest.java
@@ -0,0 +1,62 @@
+package FTP;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FTPClientTest {
+
+	static String host = "166.111.80.101";
+
+	static FTPClient client = null;
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		//System.out.println(System.getProperty("user.dir"));
+		client = new FTPClient();
+		
+		// For connection test
+		client.connect(host, "zhang", "Myzhang123");
+		String pwd = client.pwd();
+		System.out.println(pwd);
+		
+		// For List test
+		client.list();
+		client.list("incoming");
+		System.out.println("---------------------------------------");
+		if(client.cwd("incoming")){
+			System.out.println("Change the directory successfully!");
+		}else
+			System.out.println("Change the directory failed!");
+		client.list("conf-out.xml");
+//		client.cdup();
+//		System.out.println(client.pwd());
+//		client.list("conf-out.xml");
+		
+		// For download test
+		client.download("conf-out.xml");
+		client.download("conf-out1.xml");
+		client.download("site-1.4.7.zip");
+
+		// For file upload test
+//		File file = new File("FTPClient.java");
+//		client.stor(file);
+//		
+//		file = new File("sogou_pxp.exe");
+//		client.stor(file);
+		
+		// For file delete test
+//		client.delete("FTPClient.java");
+		
+		// For directory test
+		client.mkDirectory("hello");
+		client.rmDirectory("hello");
+		client.rename("conf-out.xml", "conf.xml");
+		
+		client.disconnect();
+
+	}
+
+}
